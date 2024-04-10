cba1928d670016c64cc76e7c29fd816d67e6f871
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-04-10 11:05:37 +0300

    all project

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 from src.masks import account_mask, card_mask
+
 """
 Импортируем ранее сделанные функции
 """
@@ -21,3 +22,29 @@ def datetime_and_date(date_time: str) -> str:
     """
     date_divided = date_time.split("T")[0].split("-")
     return f"{date_divided[2]}.{date_divided[1]}.{date_divided[0]}"
+
+
+def mask_phone_numbers(text: str) -> str:
+    """
+    Функция, чтобы маскировать номера телефона в предложениях
+    """
+    words = text.split(" ")
+    masked_text = []
+
+    for word in words:
+        if len(word) == 12 and word[0] == "+" and word[1:].isdigit():
+            masked_word = word[:8] + "XXXX"
+            masked_text.append(masked_word)
+        elif len(word) == 13 and word[0] == "+" and word[-1] == ",":
+            masked_word = word[:8] + "XXXX,"
+            masked_text.append(masked_word)
+        elif len(word) == 13 and word[0] == "+" and word[-1] == ".":
+            masked_word = word[:8] + "XXXX."
+            masked_text.append(masked_word)
+        elif len(word) == 14 and word[0] == "+" and word[-1] == '"':
+            masked_word = word[:8] + 'XXXX."'
+            masked_text.append(masked_word)
+        else:
+            masked_text.append(word)
+
+    return " ".join(masked_text)
--- tests/tester.py
+++ tests/tester.py
@@ -1,6 +1,9 @@
-from src.widget import datetime_and_date, masks_of_cards
+from src.widget import datetime_and_date, mask_phone_numbers, masks_of_cards
 
 cards_input = input("Введите номер карты/счета:")
 print(masks_of_cards(cards_input))
 data_input = input("Введите дату:")
 print(datetime_and_date(data_input))
+input_texts = input("Как с вами связаться? :")
+masked_text = mask_phone_numbers(input_texts)
+print(masked_text)
