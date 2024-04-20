5c235363fbb4652c0769ac329cd48cb70a595d92
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-04-20 12:41:52 +0300

    Adding a Type Annotation

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,17 +4,16 @@ from src.masks import account_mask, card_mask
 
 
 @pytest.fixture
-def bill_num():
+def bill_num() -> str:
     return "1234567890"
 
 
-def test_account_mask(bill_num):
+def test_account_mask(bill_num: str) -> None:
     assert account_mask(bill_num) == "**7890"
 
 
-@pytest.mark.parametrize("card_num, mask_card_num", [
-    ("1234567887654321", "1234 56** **** 4321"),
-    ("1111111111111111", "1111 11** **** 1111")
-])
-def test_card_mask(card_num, mask_card_num):
+@pytest.mark.parametrize(
+    "card_num, mask_card_num", [("1234567887654321", "1234 56** **** 4321"), ("1111111111111111", "1111 11** **** 1111")]
+)
+def test_card_mask(card_num: str, mask_card_num: str) -> None:
     assert card_mask(card_num) == mask_card_num
--- tests/test_winget.py
+++ tests/test_winget.py
@@ -1,20 +1,26 @@
 from src.widget import dates, mask_phone_numbers, masks_of_cards
 
 
-def test_dates():
+def test_dates() -> None:
     assert dates("2018-07-11T02:26:18.671407") == "11.07.2018"
 
 
-def test_mask_phone_numbers():
-    assert (mask_phone_numbers("Мой номер: +79112345678, позвоните мне, пожалуйста.") ==
-            "Мой номер: +7911234XXXX, позвоните мне, пожалуйста.")
-    assert (mask_phone_numbers("Звоните по телефону +79115556677 или пишите на номер +79118887766.") ==
-            "Звоните по телефону +7911555XXXX или пишите на номер +7911888XXXX.")
+def test_mask_phone_numbers() -> None:
+    assert (
+        mask_phone_numbers("Мой номер: +79112345678, позвоните мне, пожалуйста.")
+        == "Мой номер: +7911234XXXX, позвоните мне, пожалуйста."
+    )
+    assert (
+        mask_phone_numbers("Звоните по телефону +79115556677 или пишите на номер +79118887766.")
+        == "Звоните по телефону +7911555XXXX или пишите на номер +7911888XXXX."
+    )
 
 
-def test_masks_of_cards():
+def test_masks_of_cards() -> None:
     assert masks_of_cards("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
     assert masks_of_cards("Счет 73654108430135874305") == "Счет **4305"
+
+
 # cards_number_input = input("Введите номер карты/счета:")
 # print(masks_of_cards(cards_number_input))
 #
