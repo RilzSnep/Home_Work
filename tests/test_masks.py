fb1e0779c461775cee36ec4c6678fdc3226decca
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-04-20 12:30:52 +0300

    renaming and improving tests

---
+++ tests/test_masks.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.masks import account_mask, card_mask
+
+
+@pytest.fixture
+def bill_num():
+    return "1234567890"
+
+
+def test_account_mask(bill_num):
+    assert account_mask(bill_num) == "**7890"
+
+
+@pytest.mark.parametrize("card_num, mask_card_num", [
+    ("1234567887654321", "1234 56** **** 4321"),
+    ("1111111111111111", "1111 11** **** 1111")
+])
+def test_card_mask(card_num, mask_card_num):
+    assert card_mask(card_num) == mask_card_num
---
+++ tests/test_winget.py
@@ -0,0 +1,26 @@
+from src.widget import dates, mask_phone_numbers, masks_of_cards
+
+
+def test_dates():
+    assert dates("2018-07-11T02:26:18.671407") == "11.07.2018"
+
+
+def test_mask_phone_numbers():
+    assert (mask_phone_numbers("Мой номер: +79112345678, позвоните мне, пожалуйста.") ==
+            "Мой номер: +7911234XXXX, позвоните мне, пожалуйста.")
+    assert (mask_phone_numbers("Звоните по телефону +79115556677 или пишите на номер +79118887766.") ==
+            "Звоните по телефону +7911555XXXX или пишите на номер +7911888XXXX.")
+
+
+def test_masks_of_cards():
+    assert masks_of_cards("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
+    assert masks_of_cards("Счет 73654108430135874305") == "Счет **4305"
+# cards_number_input = input("Введите номер карты/счета:")
+# print(masks_of_cards(cards_number_input))
+#
+# data_input = input("Введите дату:")
+# print(dates(data_input))
+#
+# input_texts = input("Как с вами связаться? :")
+# masked_text = mask_phone_numbers(input_texts)
+# print(masked_text)
